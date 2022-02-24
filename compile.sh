ca4bb8db3355926739af5a141b011cb00ddcecb5
Courtney Trom <ctrom2@illinois.edu>
2022-02-24 13:09:17 -0600

    mode test

--- compile.sh
+++ compile.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-#g++ -lm -o FullTest rocketlander.cpp MS5611.cpp Util.cpp I2Cdev.cpp MPU9250.cpp LSM9DS1.cpp PWM.cpp RCOutput_Navio2.cpp
-g++ -lm -o modeTest gimbalModeTest.cpp MS5611.cpp Util.cpp I2Cdev.cpp MPU9250.cpp LSM9DS1.cpp PWM.cpp RCOutput_Navio2.cpp
+g++ -lm -o FullTest rocketlander.cpp MS5611.cpp Util.cpp I2Cdev.cpp MPU9250.cpp LSM9DS1.cpp PWM.cpp RCOutput_Navio2.cpp
+#g++ -lm -o modeTest gimbalModeTest.cpp MS5611.cpp Util.cpp I2Cdev.cpp MPU9250.cpp LSM9DS1.cpp PWM.cpp RCOutput_Navio2.cpp
+
+
